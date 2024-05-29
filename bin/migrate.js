5addab0e77ccfb9e19aa2573743760862c96f606
Rishi Raj Jain <jain71000@gmail.com>
2024-05-29 23:54:00 +0530

    file path

--- bin/migrate.js
+++ bin/migrate.js
@@ -5,9 +5,10 @@ import { migrateData } from '../src/index.js'
 
 program
   .option('--silent <silent>', 'Suppress console output')
-  .option('--source <source>', 'Source PostgreSQL connection string')
   .option('--accept-all <accept-all>', 'Automatically accept all prompts')
+  .option('--source <source>', 'Source PostgreSQL connection string')
   .option('--destination <destination>', 'Destination PostgreSQL connection string')
+  .option('--backup-file-path <backup-file-path>', 'Path of the backup file name')
 
 program.parse(process.argv)
 
@@ -17,5 +18,6 @@ process.env.SILENT = options.silent
 process.env.ACCEPT_ALL = options.acceptAll
 process.env.SOURCE_CONNECTION_STRING = options.source
 process.env.DESTINATION_CONNECTION_STRING = options.destination
+process.env.BACKUP_FILE_PATH = options.backupFilePath
 
 migrateData()
--- src/index.js
+++ src/index.js
@@ -72,7 +72,7 @@ export async function migrateData() {
   console.log(`[@neondatabase/pg-import] Setting pg_dump and pg_restore for Postgres ${postgresVersion}`)
   await setupPgDumpAndPgRestore(postgresVersion)
   console.log('[@neondatabase/pg-import] pg_dump and pg_restore setup complete.')
-  const dumpName = `dump_restore_${uuidv4()}.bak`
+  const dumpName = process.env.BACKUP_FILE_PATH ?? `dump_restore_${uuidv4()}.bak`
   executeCommandSync('pg_dump', ['-Fc', '-v', '-d', process.env.SOURCE_CONNECTION_STRING, '-f', dumpName])
   console.log(`[@neondatabase/pg-import] Created a backup file '${dumpName}' succesfully.`)
   executeCommandSync('pg_restore', ['-v', '-d', process.env.DESTINATION_CONNECTION_STRING, dumpName])

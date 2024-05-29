eacf6c9e3685f569b806446714feb3dd70752c2d
Rishi Raj Jain <jain71000@gmail.com>
2024-05-29 19:35:44 +0530

    fix: remove inherit

--- src/subcommand.js
+++ src/subcommand.js
@@ -1,18 +1,17 @@
 import { spawnSync } from 'child_process'
 
 export default function executeCommandSync(command, args, options) {
-  const result = spawnSync(command, args, { ...options, stdio: ['pipe', 'pipe', 'inherit'] })
-  const stdout = result.stdout ? result.stdout.toString() : ''
-  const stderr = result.stderr ? result.stderr.toString() : ''
+  const result = spawnSync(command, args, { ...options, stdio: 'pipe' })
+  const stdout = result.stdout?.toString() ?? ''
+  const stderr = result.stderr?.toString() ?? ''
   if (result.status !== 0) {
     console.error(`Command '${command}' failed with code ${result.status}`)
     console.error(stderr)
   }
-  if (process.env.SILENT !== 'true') {
-    if (!Boolean(options?.silent)) {
-      console.log(stdout)
-      console.error(stderr)
-    }
+  const displayLog = process.env.SILENT !== 'true' && !Boolean(options?.silent)
+  if (displayLog) {
+    console.log(stdout)
+    console.error(stderr)
   }
   return { stdout, stderr }
 }

cbaca8a7c640f7f5862802a4248482fd6d0db50f
Rishi Raj Jain <jain71000@gmail.com>
2024-06-04 00:36:02 +0530

    feat: use pg

--- src/version.js
+++ src/version.js
@@ -1,8 +1,10 @@
-import { neon } from '@neondatabase/serverless'
+import pg from 'pg'
+const { Client } = pg
 
 export default async function getPostgresVersion(connectionString) {
-  const sql = neon(connectionString)
-  const rows = await sql`SHOW server_version`
+  const client = new Client({ connectionString })
+  await client.connect()
+  const { rows } = await client.query('SHOW server_version')
   const version = rows[0].server_version
   return version.substring(0, version.indexOf('.'))
 }
